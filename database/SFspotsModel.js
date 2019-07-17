c6fd6cdf6b8ddc8052bc07f165778216353522f8
sagdish <forsi1@yahoo.com>
2019-07-16 17:47:00 -0700

    routes and database directories create, \n mongodb connection using mongoose package, \n create SFspotsDB database, \n create schema and model for SFspotsDB

---
+++ database/SFspotsModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const definition = {
+  name: {
+    type: String,
+    required: true,
+  },
+  position: {
+    lat: Number,
+    lng: Number,
+  },
+  rating: {
+    type: Number
+  },
+  photos: [{
+    ref: String
+  }],
+};
+
+const options = {
+  timestamps: true,
+};
+
+// Fix DeprecationWarning: collection.ensureIndex is deprecated. Use createIndexes instead
+mongoose.set('useCreateIndex', true);
+
+const sfSpotsSchema = new mongoose.Schema(definition, options);
+const sfSpotsModel = mongoose.model(Spot, sfSpotsSchema);
+
+module.exports = sfSpotsModel;
--- server.js
+++ server.js
@@ -4,19 +4,35 @@ const cors = require('cors');
 const axios = require('axios');
 const mongoose = require('mongoose');
 
-const server = express();
+// database connection
+mongoose.connect('mongodb://localhost/SFspotsDB', { useNewUrlParser: true })
+  .then(mongo => {
+    console.log("\n === connected to 'SFspotsDB' database")
+  })
+  .catch(err => {
+    console.log('error connecting to mongdo', err);
+  });
 
+// server instantiation
+const server = express();
 server.use(helmet());
 server.use(express.json());
+
+// prevent CORS errors
 server.use(cors());
 
 
 // test Api call for a single photo:
 //`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=cafe%20enchante&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=${apiKey}`
 
+// test api:
+server.get('/test', (req, res) => {
+  res.json('API is running');
+})
 
 // Get the first photo:
 server.get('/', (req, res) => {
+
 })
 
 
